fc515aa733161a4557b83bee0f1ee84a4cfb74d5
Archit Agarwal <archit.agarwal@walmartlabs.com>
2020-04-19 18:56:06 -0700

    adding order now button

--- src/containers/BurgerBuilder/BugerBuilder.js
+++ src/containers/BurgerBuilder/BugerBuilder.js
@@ -19,7 +19,20 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState = ingredients => {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, curr) => {
+        return sum + curr; 
+      }, 0);
+    
+    this.setState({purchasable: sum > 0});
   }
 
   addIngredientHandler = type => {
@@ -33,6 +46,7 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredients,
       totalPrice: newPrice
     });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = type => {
@@ -47,6 +61,7 @@ class BurgerBuilder extends Component {
         ingredients: updatedIngredients,
         totalPrice: newPrice
       });
+      this.updatePurchaseState(updatedIngredients);
     }
   }
 
@@ -64,6 +79,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
         />
       </Aux>
     )

091e0eb3c623aacaa53ec7f79083f9a3b268d293
Archit Agarwal <archit.agarwal@walmartlabs.com>
2020-05-04 15:06:52 -0700

    tried deleting cached files

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 import classes from "./Modal.css";
+import Aux from "../../../hoc/Aux";
+import Backdrop from "../Backdrop/Backdrop"; 
 
 const Modal = props => {
-  const { show } = props;
+  const {
+    show,
+    modalClosed
+  } = props;
   return (
-  <div
-    className={classes.Modal}
-    style={{
-      transform: show ? "translateY(0)" : "translateY(-100vh)",
-      opacity: show ? "1" : "0"
-    }}>
-    {props.children}
-  </div>
+    <Aux>
+      <Backdrop
+        show={show}
+        clicked={modalClosed}/>
+      <div
+        className={classes.Modal}
+        style={{
+          transform: show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: show ? "1" : "0"
+        }}>
+        {props.children}
+      </div>
+    </Aux>
 )};
 
 export default Modal;

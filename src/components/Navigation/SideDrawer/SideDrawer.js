62e0904fff903c0522bcbe4c5c58c4bc25e650b4
Archit Agarwal <archit.agarwal@walmartlabs.com>
2020-06-02 16:32:54 -0700

    burger builder basic complete

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux/Aux";
+import classes from "./SideDrawer.css";
+
+const SideDrawer = props => {
+  const attachedClasses = [classes.SideDrawer, props.open ? classes.Open : classes.Close]
+  return (
+    <Aux>
+      <Backdrop
+        show={props.open}
+        clicked={props.closed}
+        />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+    </div>
+    </Aux>
+  );
+};
+
+export default SideDrawer;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
 
 import classes from "./Modal.css";
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop"; 
 
-const Modal = props => {
-  const {
-    show,
-    modalClosed
-  } = props;
-  return (
-    <Aux>
-      <Backdrop
-        show={show}
-        clicked={modalClosed}/>
-      <div
-        className={classes.Modal}
-        style={{
-          transform: show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: show ? "1" : "0"
-        }}>
-        {props.children}
-      </div>
-    </Aux>
-)};
+class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render () {
+    const {
+      show,
+      modalClosed
+    } = this.props;
+
+    return (
+      <Aux>
+        <Backdrop
+          show={show}
+          clicked={modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: show ? "1" : "0"
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  };
+};
 
 export default Modal;
